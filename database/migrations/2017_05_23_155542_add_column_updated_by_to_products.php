7754ce2152a7965ad039abbf7aaed17a0006baa5
ChinhFong <hangchinhfong@gmail.com>
2017-05-23 16:00:41 +0700

    add updated_by to products table

---
+++ database/migrations/2017_05_23_155542_add_column_updated_by_to_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnUpdatedByToProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('products', function($table)
+        {
+            $table->string('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('products', function($table)
+        {
+            $table->dropColumn('updated_by');
+        });
+    }
+}
